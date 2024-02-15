0faad981c487e2281cfa68d4c517868c9ac3ea55
joshua daza <joshuadazar@gmail.com>
2024-02-15 18:51:42 -0500

    feature/create hero banner and set responsive behaviors

--- angular-movies/src/app/components/header/header.component.ts
+++ angular-movies/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CtaComoponentComponent } from '../cta-comoponent/cta-comoponent.component';
+import { NavComponentComponent } from '../nav-component/nav-component.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CtaComoponentComponent],
+  imports: [CtaComoponentComponent, NavComponentComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
