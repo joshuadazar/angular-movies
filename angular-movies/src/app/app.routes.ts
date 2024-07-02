11f617cedbbcc51c63f252230bceb086e9f6f4de
joshua daza <joshuadazar@gmail.com>
2024-07-02 10:49:44 -0500

    feature/add state component

--- angular-movies/src/app/app.routes.ts
+++ angular-movies/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { ThreeComponent } from './components/three/three.component';
 import { ThreeExample2Component } from './components/three-example2/three-example2.component';
+import { State1Component } from './components/state1/state1.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'three2',
     component: ThreeExample2Component
+  },
+  {
+    path: 'state',
+    component: State1Component
   }
 ];
---
+++ angular-movies/src/app/components/state1/state1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-state1',
+  standalone: true,
+  imports: [],
+  templateUrl: './state1.component.html',
+  styleUrl: './state1.component.scss'
+})
+export class State1Component {
+
+}
